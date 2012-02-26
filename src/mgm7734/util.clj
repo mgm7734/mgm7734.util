e8aa40e871964655c63dc0f6e99ad4e06f0683d5
Mark <Mark@Mark-Tower>
2012-02-25 22:32:41 -0600

    match-binds and match?

--- src/mgm7734/util.clj
+++ src/mgm7734/util.clj
@@ -24,41 +24,87 @@
       (cond (nil? xs) (cmp n 0)
             (== 0 n)  (cmp 0 1)
             :else (recur (dec n) (next xs))))
-    ))        
+    ))
 
 (defn- match-vec
   [pat expr]
-  (condl :let [[pos-pats rest-pats] (split-with #(not= '& %) pat)]
-                              
+  (condl (empty? pat) `(= ~pat ~expr)
+         :let [[pos-pats rest-pats] (split-with #(not= '& %) pat)]                              
          (not (#{0 2} (count rest-pats))) 
          (throw (IllegalArgumentException. (str "& must be followed by exact one form, not " 
                                                 rest-pats)))
-         :let [pos-clauses  (map-indexed (fn [i p] `(match? ~p (get ~expr ~i))) pos-pats)
+         :let [e (gensym "e")
+               pos-clauses  (map-indexed (fn [i p] `(match? ~p (get ~e ~i))) pos-pats)
                rest-clauses (if rest-pats
-                              [`(match? ~(second rest-pats) (nthnext ~expr ~(count pos-pats)))])]
+                              [`(match? ~(second rest-pats) (nthnext ~e ~(count pos-pats)))])]
          true
-         `(and (sequential? ~expr) (count-bound ~(count pos-pats) <= ~expr)
-               ~@pos-clauses ~@rest-clauses) ))
+         `(let [~e ~expr] 
+            (and (sequential? ~e) (count-bound ~(count pos-pats) <= ~e)
+                 ~@pos-clauses ~@rest-clauses)) ))
 
 (defmacro match?
-  "pat => {map-pat*} , [pos-pat* rest-pat?] , (pred-pat) , #\"re-pat\" , anything-pat , 'lit-sym-pat, or const-pat
+  "Pattern matching test without bindings.  This can be a lot more efficient than match-binds or match-let
+ pat => {map-pat*} , [pos-pat* rest-pat?] , 'quote-pat, (pred-pat) , #\"re-pat\" , anything-pat , sym-pat or const-pat
  map-pat => key pat ,        matches if expr is a map and pat matches (get expr key)
  pos-pat => pat ,            matches if expr is sequential with values matching every corresponding pat
  rest-pat => & pat ,         matches the rest of a sequential expr
  pred-pat => pred args*      matches if (pred arg* expr) is true
  re-pat  =>                  matches strings
- anything-pat => :_          equivalent to [but more efficient than] the pattern ((constantly true)) 
- lit-sym-pat  => symbol      literal match.  unquoted reserved
+ anything-pat => _           equivalent to [but more efficient than] the pattern ((constantly true)) 
+ quote-pat  => anything      literal match. 
+ sym-pat => symbol ,         is an error! Variables only allowed in match-binds and match-let
  const-pat => anything else, matches when = expr. Note that the pattern is not evaluated, but the expr is.
 "
   [pat expr]
   (cond
-    (map?        pat)  `(and (map? ~expr) ~@(for [[k p] pat] `(match? ~p (get ~expr ~k))) )
+    (map?        pat)  (let [e (gensym "e")] 
+                         `(let [~e ~expr] 
+                            (and (map? ~e) ~@(for [[k p] pat] `(match? ~p (get ~e ~k))) )))
     (vector?     pat)   (match-vec pat expr)    
     (and (sequential? pat) (not= 'quote (first pat))) 
                        (concat pat [expr])
-    (= ':_       pat)   true
+    (= '_       pat)   true
     (instance? java.util.regex.Pattern pat)
                        `(re-matches ~pat ~expr)
     (symbol? pat)       (throw (IllegalArgumentException. (str pat ": naked symbols not allowed (yet)")))
     :else              `(= ~pat ~expr) ))
+
+(defmacro and-match-binds [& mes]
+  (let [step (fn step [mes rs]
+               (if (empty? mes) 
+                 (cons 'concat rs)
+                 (let [r (gensym "r")] 
+                   `(if-let [~r ~(first mes)] ~(step (rest mes) (conj rs r))))))]
+    (step mes [])) )
+
+(defn- match-binds-vec
+  [pat expr]
+  (condl (empty? pat) `(if (= ~pat ~expr) [])
+         :let [[pos-pats rest-pats] (split-with #(not= '& %) pat)]                              
+         (not (#{0 2} (count rest-pats))) 
+         (throw (IllegalArgumentException. (str "& must be followed by exact one form, not " 
+                                                rest-pats)))
+         :let [e (gensym "e")
+               pos-clauses  (map-indexed (fn [i p] `(match-binds ~p (get ~e ~i))) pos-pats)
+               rest-clauses (if rest-pats
+                              [`(match-binds ~(second rest-pats) (nthnext ~e ~(count pos-pats)))])]
+         true
+         `(let [~e ~expr] 
+            (and (sequential? ~e) (count-bound ~(count pos-pats) <= ~e)
+                 (and-match-binds ~@pos-clauses ~@rest-clauses))) ))
+
+(defmacro match-binds
+  "Pattern matching with vars"
+  [pat expr]
+  (cond
+    (= '_        pat)   []
+    (symbol? pat)       `['~pat ~expr]
+    (map?        pat)  (let [e (gensym "e")] 
+                         `(let [~e ~expr] 
+                            (and (map? ~e) (and-match-binds ~@(for [[k p] pat] `(match-binds ~p (get ~e ~k)))) )))
+    (vector?     pat)   (match-binds-vec pat expr)    
+    (and (sequential? pat) (not= 'quote (first pat))) 
+                       `(if ~(concat pat [expr]) [])
+    (instance? java.util.regex.Pattern pat)
+                       `(if (re-matches ~pat ~expr) [])
+    :else              `(if (= ~pat ~expr) []) ))
--- test/mgm7734/test/util.clj
+++ test/mgm7734/test/util.clj
@@ -2,6 +2,8 @@
   (:use [clojure.test]
         [mgm7734.util]))
 
+(def ^:dynamic cnt)
+
 (deftest match?-tests
 	(is      (match? 1 1))
 	(is (not (match? 0                1)))
@@ -9,12 +11,53 @@
 	(is (not (match? {:a 0}           {:a 1 :b 2})))
 	(is      (match? {:a (even?)}     {:a 2}))
 	(is (not (match? {:a (even?)}     {:a 1})))
-	(is      (match? :_               {:foo '(will match? anything)}))
+	(is      (match? _                {:foo '(will match? anything)}))
 	(is      (match? [(even?) 1]      [2 1])) 
 	(is (not (match? [(even?) 1]      [2 1 0])))
-	(is      (match? [(even?) 1 & :_] [2 1 0 -1])) 
+	(is      (match? [(even?) 1 & _]  [2 1 0 -1])) 
 	
 	(is      (match? #"H.*!"          "Hello, world!"))
    
 ;   (is (= :ok (try (macroexpand '(match? a 1)) (catch IllegalArgumentException e :ok))))
-  )
+
+(binding [cnt 0]
+  (defn expensive [] 
+    (set! cnt (inc cnt)) 
+    {:a 1, :b 2})
+  (is (match? {:a 1, :b 2} (expensive)))
+  (is (= 1 cnt)))
+
+(binding [cnt 0]
+  (defn expensive [] 
+    (set! cnt (inc cnt)) 
+    [1 2 3])  
+  (is (match? [1 2 3] (expensive)))
+  (is (= 1 cnt)))
+
+   (is (= ['a 3] (match-binds a                  (+ 1 2))))
+   (is (= []     (match-binds 1                  1)))
+   (is (not      (match-binds 0                  1)))
+	(is (= []     (match-binds '(anything quoted) '(anything quoted))))       
+	(is (not      (match-binds {:a 0}             {:a 1 :b 2})))
+	(is (= []     (match-binds {:a (even?)}       {:a 2})))
+	(is (not      (match-binds {:a (even?)}       {:a 1})))
+	(is (= []     (match-binds _                  {:foo '(will match? anything)})))
+	(is (= ['x 1] (match-binds [(even?) x]        [2 1]))) 
+	(is (not      (match-binds [(even?) x]        [2 1 0]))) 
+	(is (= ['x 1 'y [0 -1]] (match-binds [(even?) x & y]    [2 1 0 -1]))) 
+	(is (= []     (match-binds #"H.*!"          "Hello, world!")))
+  
+ (binding [cnt 0]
+  (defn expensive [] 
+    (set! cnt (inc cnt)) 
+    {:a 1, :b 2})
+  (is (match-binds {:a 1, :b 2} (expensive)))
+  (is (= 1 cnt)))
+
+(binding [cnt 0]
+  (defn expensive [] 
+    (set! cnt (inc cnt)) 
+    [1 2 3])  
+  (is (match-binds [1 2 3] (expensive)))
+  (is (= 1 cnt)))
+)
